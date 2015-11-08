7e346cfcb877d44bb8976c2dfa98f1d7e0bcc72b
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2015-11-08 15:25:57 -0500

    update license terms
    * removed OpenSSL exception
      (OpenDHT doesn't make use of OpenSSL)
    * properly capitalize "Savoir-faire Linux"

--- include/opendht/securedht.h
+++ include/opendht/securedht.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014-2015 Savoir-Faire Linux Inc.
+ *  Copyright (C) 2014-2015 Savoir-faire Linux Inc.
  *  Author : Adrien Béraud <adrien.beraud@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -21,7 +21,7 @@
  *  If you modify this program, or any covered work, by linking or
  *  combining it with the OpenSSL project's OpenSSL library (or a
  *  modified version of that library), containing parts covered by the
- *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
+ *  terms of the OpenSSL or SSLeay licenses, Savoir-faire Linux Inc.
  *  grants you additional permission to convey the resulting work.
  *  Corresponding Source for a non-source form of such a combination
  *  shall include the source code for the parts of OpenSSL used as well
--- src/infohash.cpp
+++ src/infohash.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2014-2015 Savoir-Faire Linux Inc.
+ *  Copyright (C) 2014-2015 Savoir-faire Linux Inc.
  *  Author : Adrien Béraud <adrien.beraud@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -15,17 +15,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
- *
- *  Additional permission under GNU GPL version 3 section 7:
- *
- *  If you modify this program, or any covered work, by linking or
- *  combining it with the OpenSSL project's OpenSSL library (or a
- *  modified version of that library), containing parts covered by the
- *  terms of the OpenSSL or SSLeay licenses, Savoir-Faire Linux Inc.
- *  grants you additional permission to convey the resulting work.
- *  Corresponding Source for a non-source form of such a combination
- *  shall include the source code for the parts of OpenSSL used as well
- *  as that of the covered work.
  */
 
 #include "infohash.h"
