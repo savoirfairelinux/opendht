dd107e6f6d44819687d039ce5829e227e50d7747
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-01-09 18:05:42 -0500

    python: fix licenses terms

--- python/opendht.pyx
+++ python/opendht.pyx
@@ -14,19 +14,18 @@
 #
 # This file is part of OpenDHT Python Wrapper.
 #
-#    OpenDHT Python Wrapper is free software:  you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# OpenDHT Python Wrapper is free software:  you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    OpenDHT Python Wrapper is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# OpenDHT Python Wrapper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with OpenDHT Python Wrapper. If not, see <http://www.gnu.org/licenses/>.
-#
+# You should have received a copy of the GNU General Public License
+# along with OpenDHT Python Wrapper. If not, see <http://www.gnu.org/licenses/>.
 
 from libcpp.map cimport map as map
 from libcpp cimport bool
