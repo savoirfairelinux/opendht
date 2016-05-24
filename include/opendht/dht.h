564e97fce74a871b0118dc86dacbfb43bf51ad61
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2016-05-24 03:09:41 -0400

    dht: use 4 listen nodes

--- include/opendht/dht.h
+++ include/opendht/dht.h
@@ -263,7 +263,7 @@ private:
     static constexpr unsigned SEARCH_REQUESTS {4};
 
     /* Number of listening nodes */
-    static constexpr unsigned LISTEN_NODES {3};
+    static constexpr unsigned LISTEN_NODES {4};
 
     /* The maximum number of values we store for a given hash. */
     static constexpr unsigned MAX_VALUES {1024};
