fb920e5ee0d3f035e5a4eb5e7dff88456e54244d
Nicolas <kaldoran@users.noreply.github.com>
2016-08-01 15:28:08 -0400

    dht: prevent outdated store of value

--- src/dht.cpp
+++ src/dht.cpp
@@ -1670,6 +1670,10 @@ Dht::storageStore(const InfoHash& id, const std::shared_ptr<Value>& value, time_
 {
     const auto& now = scheduler.time();
     created = std::min(created, now);
+
+    if (created + getType(value->id).expiration < now)
+        return false;
+
     auto st = findStorage(id);
     if (st == store.end()) {
         if (store.size() >= MAX_HASHES)
