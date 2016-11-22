e7db64bd7428424670358ec7127aa8d69cae68d3
Istvan <istvan.madari#vanderbilt.edu>
2016-11-22 14:04:11 -0600

    Log functions are moved to log.cpp
    Log functions are moved to the log.cpp
    in order to avoid multiple definitions
    of the logger functions.

--- include/opendht/log.h
+++ include/opendht/log.h
@@ -62,57 +62,19 @@ constexpr const Color::Modifier yellow(Color::FG_YELLOW);
 /**
  * Print va_list to std::ostream (used for logging).
  */
-void
-printLog(std::ostream& s, char const* m, va_list args) {
-    // print log to buffer
-    std::array<char, 8192> buffer;
-    int ret = vsnprintf(buffer.data(), buffer.size(), m, args);
-    if (ret < 0)
-        return;
+OPENDHT_PUBLIC void
+printLog(std::ostream &s, char const *m, va_list args);
 
-    // write timestamp
-    using namespace std::chrono;
-    using log_precision = microseconds;
-    constexpr auto den = log_precision::period::den;
-    auto micro = duration_cast<log_precision>(steady_clock::now().time_since_epoch()).count();
-    s << "[" << std::setfill('0') << std::setw(6) << micro / den << "." 
-             << std::setfill('0') << std::setw(6) << micro % den << "]" << " ";
+OPENDHT_PUBLIC void
+enableLogging(dht::DhtRunner &dht);
 
-    // write log
-    s.write(buffer.data(), std::min((size_t)ret, buffer.size()));
-    if ((size_t)ret >= buffer.size())
-        s << "[[TRUNCATED]]";
-    s << std::endl;
-}
+OPENDHT_PUBLIC void
+enableFileLogging(dht::DhtRunner &dht, const std::string &path);
 
-void
-enableLogging(dht::DhtRunner& dht)
-{
-    dht.setLoggers(
-        [](char const* m, va_list args){ std::cerr << red; printLog(std::cerr, m, args); std::cerr << def; },
-        [](char const* m, va_list args){ std::cout << yellow; printLog(std::cout, m, args); std::cout << def; },
-        [](char const* m, va_list args){ printLog(std::cout, m, args); }
-    );
-}
+OPENDHT_PUBLIC void
+disableLogging(dht::DhtRunner &dht);
 
-void
-enableFileLogging(dht::DhtRunner& dht, const std::string& path)
-{
-    auto logfile = std::make_shared<std::fstream>();
-    logfile->open(path, std::ios::out);
 
-    dht.setLoggers(
-        [=](char const* m, va_list args){ printLog(*logfile, m, args); },
-        [=](char const* m, va_list args){ printLog(*logfile, m, args); },
-        [=](char const* m, va_list args){ printLog(*logfile, m, args); }
-    );
-}
-
-void
-disableLogging(dht::DhtRunner& dht)
-{
-    dht.setLoggers(dht::NOLOG, dht::NOLOG, dht::NOLOG);
-}
 
 } /* log */
 } /* dht  */
