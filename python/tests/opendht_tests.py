4d78aea05b5e4ce1f6f1fbacf3b1b79ab415c2a1
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2017-04-27 03:24:12 +0200

    unit tests: add crypto unit tests

--- python/tests/opendht_tests.py
+++ python/tests/opendht_tests.py
@@ -16,7 +16,38 @@ class OpenDhtTester(unittest.TestCase):
         a.run()
         b = dht.DhtRunner()
         b.run()
-        self.assertTrue(b.bootstrap(a.getBound()))
+        self.assertTrue(b.ping(a.getBound()))
+
+    def test_crypto(self):
+        i = dht.Identity.generate("id")
+        message = dht.InfoHash.getRandom().toString()
+        encrypted = i.publickey.encrypt(message)
+        decrypted = i.key.decrypt(encrypted)
+        self.assertTrue(message == decrypted)
+
+    def test_crypto_ec(self):
+        key = dht.PrivateKey.generateEC()
+        cert = dht.Certificate.generate(key, "CA", is_ca=True)
+        ca_id = dht.Identity(key, cert)
+        self.assertTrue(cert.getId() == key.getPublicKey().getId())
+        key2 = dht.PrivateKey.generateEC()
+        cert2 = dht.Certificate.generate(key2, "cert", ca_id)
+        trust = dht.TrustList()
+        trust.add(cert)
+        self.assertTrue(trust.verify(cert2))
+
+    def test_trust(self):
+        main_id = dht.Identity.generate("id_1")
+        sub_id1 = dht.Identity.generate("sid_1", main_id)
+        sub_id2 = dht.Identity.generate("sid_2", main_id)
+        main_id.certificate.revoke(main_id.key, sub_id2.certificate)
+        main_id2 = dht.Identity.generate("id_2")
+        trust = dht.TrustList()
+        trust.add(main_id.certificate)
+        self.assertTrue(trust.verify(main_id.certificate))
+        self.assertTrue(trust.verify(sub_id1.certificate))
+        self.assertFalse(trust.verify(sub_id2.certificate))
+        self.assertFalse(trust.verify(main_id2.certificate))
 
 if __name__ == '__main__':
     unittest.main()
