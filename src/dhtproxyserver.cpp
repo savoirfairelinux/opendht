80fc37625394cf3a2bc7d42470f82ca09c835284
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-10-25 17:32:40 -0400

    proxy putSigned

--- src/dhtproxyserver.cpp
+++ src/dhtproxyserver.cpp
@@ -4,7 +4,9 @@
 
 #include <functional>
 
+#include "default_types.h"
 #include "dhtrunner.h"
+#include "msgpack.hpp"
 
 namespace dht {
 
@@ -31,12 +33,31 @@ DhtProxyServer::DhtProxyServer(DhtRunner* dht) : dht_(dht)
         }
     );
     service_->publish(resource);
+    resource = std::make_shared<restbed::Resource>();
+    resource->set_path("/putSigned");
+    resource->set_method_handler("PUT",
+        [this](const std::shared_ptr<restbed::Session> session)
+        {
+            this->putSigned(session);
+        }
+    );
+    service_->publish(resource);
 
     server_thread = std::thread([this]() {
         auto settings = std::make_shared<restbed::Settings>();
-        settings->set_port(1984); // TODO add argument and move this
         settings->set_default_header("Connection", "close");
-        service_->start(settings);
+        int port = 1984;
+        int started = false;
+        while (!started)
+        {
+            try {
+                settings->set_port(port); // TODO add argument and move this
+                service_->start(settings);
+                started = true;
+            } catch (std::system_error& e) {
+                port += 1;
+            }
+        }
     });
 }
 
@@ -60,7 +81,7 @@ DhtProxyServer::getId(const std::shared_ptr<restbed::Session>& session) const
                 auto id = dht_->getId().toString();
                 s->close(restbed::OK, id, {{"Content-Length", std::to_string(id.length())}});
             } else {
-                s->close(restbed::OK, "", {{"Content-Length", "0"}});
+                s->close(restbed::NOT_FOUND, "", {{"Content-Length", "0"}});
             }
         }
     );
@@ -80,7 +101,52 @@ DhtProxyServer::getNodeId(const std::shared_ptr<restbed::Session>& session) cons
                 auto id = dht_->getNodeId().toString();
                 s->close(restbed::OK, id, {{"Content-Length", std::to_string(id.length())}});
             } else {
-                s->close(restbed::OK, "", {{"Content-Length", "0"}});
+                s->close(restbed::NOT_FOUND, "", {{"Content-Length", "0"}});
+            }
+        }
+    );
+}
+
+void
+DhtProxyServer::putSigned(const std::shared_ptr<restbed::Session>& session) const
+{
+    const auto request = session->get_request();
+
+    int content_length = std::stoi(request->get_header("Content-Length", "0"));
+
+    session->fetch(content_length,
+        [&](const std::shared_ptr<restbed::Session> s, const restbed::Bytes& b)
+        {
+            if (dht_) {
+                if(b.empty()) {
+                    std::string response("Missing parameters");
+                    s->close(restbed::BAD_REQUEST, response, {{"Content-Length", std::to_string(response.size())}});
+                } else {
+                    restbed::Bytes buf(b);
+
+                    unsigned char delimiter = static_cast<unsigned char>('\n');
+                    unsigned int idx = 0;
+                    std::vector<unsigned char> hashBuf;
+                    while(idx < buf.size() && buf[idx] != delimiter) {
+                        hashBuf.emplace_back(buf[idx]);
+                        ++idx;
+                    }
+                    ++idx;
+                    std::vector<unsigned char> valueBuf;
+                    while(idx < buf.size()) {
+                        valueBuf.emplace_back(buf[idx]);
+                        ++idx;
+                    }
+                    InfoHash hash(hashBuf.data(), hashBuf.size());
+                    msgpack::unpacked msg;
+                    msgpack::unpack(msg, reinterpret_cast<const char*>(valueBuf.data()), valueBuf.size());
+                    auto value = std::make_shared<Value>(msg.get());
+
+                    dht_->putSigned(hash, value);
+                    s->close(restbed::OK, "", {{"Content-Length", "0"}});
+                }
+            } else {
+                s->close(restbed::NOT_FOUND, "", {{"Content-Length", "0"}});
             }
         }
     );
