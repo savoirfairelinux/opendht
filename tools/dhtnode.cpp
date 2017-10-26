1413d32ee6f26840cf03675d460e98adc44d70a2
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-10-26 17:08:53 -0400

    add listen endpoint

--- tools/dhtnode.cpp
+++ tools/dhtnode.cpp
@@ -166,7 +166,7 @@ void cmd_loop(std::shared_ptr<DhtRunner>& dht, dht_params& params)
         if (op.empty())
             continue;
 
-        static const std::set<std::string> VALID_OPS {"g", "l", "cl", "il", "ii", "p", "pp", "cpp", "s", "e", "a"};
+        static const std::set<std::string> VALID_OPS {"g", "l", "cl", "il", "ii", "p", "pp", "cpp", "s", "e", "a",  "q"};
         if (VALID_OPS.find(op) == VALID_OPS.cend()) {
             std::cout << "Unknown command: " << op << std::endl;
             std::cout << " (type 'h' or 'help' for a list of possible commands)" << std::endl;
