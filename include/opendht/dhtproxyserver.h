610664f723a92c98e1fd3f62d7c83090e854c93d
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2017-10-26 13:52:49 -0400

    proxy put and putEncrypted

--- include/opendht/dhtproxyserver.h
+++ include/opendht/dhtproxyserver.h
@@ -26,9 +26,39 @@ public:
     DhtProxyServer& operator=(DhtProxyServer&& other) = default;
 
 private:
+    /**
+     * Method: GET
+     */
     void getId(const std::shared_ptr<restbed::Session>& session) const;
+    /**
+     * Method: GET
+     */
     void getNodeId(const std::shared_ptr<restbed::Session>& session) const;
+    /**
+     * Method: PUT
+     * Body: vector of unsigned char
+     * first line: InfoHash hash
+     * second line: Value value to sign and put
+     * @param session
+     */
     void putSigned(const std::shared_ptr<restbed::Session>& session) const;
+    /**
+     * Method: PUT
+     * Body: vector of unsigned char
+     * first line: InfoHash hash
+     * second line: InfoHash to
+     * third line: Value value to encrypt and put
+     * @param session
+     */
+    void putEncrypted(const std::shared_ptr<restbed::Session>& session) const;
+    /**
+     * Method: PUT
+     * Body: vector of unsigned char
+     * first line: InfoHash hash
+     * second line: Value value to put
+     * @param session
+     */
+    void put(const std::shared_ptr<restbed::Session>& session) const;
 
     std::thread server_thread {};
     std::unique_ptr<restbed::Service> service_;
