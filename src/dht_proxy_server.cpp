savoirfairelinux/opendht

commit 208758ede3410535b6ab2433f1da5c8d0553c586

dhtproxy: remove implicit std::move on a router return

src/dht_proxy_server.cpp
@@ -273,7 +273,7 @@ DhtProxyServer::createRestRouter()
     router->http_post("/key/:hash/encrypt", std::bind(&DhtProxyServer::putEncrypted, this, _1, _2));
 #endif // OPENDHT_PROXY_SERVER_IDENTITY
 
-    return std::move(router);
+    return router;
 }
 
 RequestStatus

commit 7ea40c292bb45427c0dec3f2dde3e43c8c7a922a

dhtproxy: fix indirect mem leak caused by 208758e

src/dht_proxy_server.cpp
@@ -126,7 +126,7 @@ DhtProxyServer::makeHttpServerSettings(const unsigned int max_pipelined_requests
     settings.separate_accept_and_create_connect(true);
     settings.logger(logger_);
     settings.protocol(restinio::asio_ns::ip::tcp::v6());
-    settings.request_handler(this->createRestRouter());
+    settings.request_handler(std::move(this->createRestRouter()));
     // time limits                                              // ~ 0.8 month
     std::chrono::milliseconds timeout_request(std::numeric_limits<int>::max());
     settings.read_next_http_message_timelimit(timeout_request);