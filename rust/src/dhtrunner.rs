3da06352b197a557909efd7377ebe2cca509ecae
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2020-01-03 15:14:32 -0500

    update copyright date

--- rust/src/dhtrunner.rs
+++ rust/src/dhtrunner.rs
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2019 Savoir-faire Linux Inc.
+ *  Copyright (C) 2014-2020 Savoir-faire Linux Inc.
  *  Author: Sébastien Blin <sebastien.blin@savoirfairelinux.com>
  *
  *  This program is free software; you can redistribute it and/or modify
