91709bc9998b903a5a1e32e8e9b2b36456e34951
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2020-11-24 15:59:42 -0500

    proxytester: disable https for now

--- tests/dhtproxytester.cpp
+++ tests/dhtproxytester.cpp
@@ -46,15 +46,15 @@ DhtProxyTester::setUp() {
     auto serverCAIdentity = dht::crypto::generateEcIdentity("DHT Node CA");
 
     dht::ProxyServerConfig serverConfig;
-    serverConfig.identity = dht::crypto::generateIdentity("DHT Node", serverCAIdentity);
+    //serverConfig.identity = dht::crypto::generateIdentity("DHT Node", serverCAIdentity);
     serverConfig.port = 8080;
-    serverConfig.pushServer = "127.0.0.1:8090";
+    // serverConfig.pushServer = "127.0.0.1:8090";
     serverProxy = std::make_unique<dht::DhtProxyServer>(nodeProxy, serverConfig);
 
-    clientConfig.server_ca = serverCAIdentity.second;
+    /*clientConfig.server_ca = serverCAIdentity.second;
     clientConfig.client_identity = dht::crypto::generateIdentity("DhtProxyTester");
-    clientConfig.push_node_id = "dhtnode";
-    clientConfig.proxy_server = "https://127.0.0.1:8080";
+    clientConfig.push_node_id = "dhtnode";*/
+    clientConfig.proxy_server = "http://127.0.0.1:8080";//"https://127.0.0.1:8080";
 }
 
 void
