53cd5817332ddc2cb3333e68d7cb5bd90db59265
Adrien Béraud <>
2024-03-17 14:13:39 -0400

    http: use OCSP_check_validity to check OCSP validity

--- src/compat/os_cert.cpp
+++ src/compat/os_cert.cpp
@@ -17,7 +17,6 @@
  */
 
 #include "os_cert.h"
-
 #include "logger.h"
 
 #ifdef _WIN32
@@ -31,96 +30,6 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <Security/Security.h>
 #endif /*TARGET_OS_OSX*/
-
-#if EMBEDDED_ASN1_TIME_PARSE
-#undef X509_NAME
-#undef OCSP_REQUEST
-#undef OCSP_RESPONSE
-#define GENTIME_LENGTH 15
-#define UTCTIME_LENGTH 13
-#define ATOI2(ar)      ((ar) += 2, ((ar)[-2] - '0') * 10 + ((ar)[-1] - '0'))
-extern "C" {
-// Taken from libressl/src/crypto/asn1/a_time_tm.c
-int
-ASN1_time_parse(const char* bytes, size_t len, struct tm* tm, int mode)
-{
-    size_t i;
-    int type = 0;
-    struct tm ltm;
-    struct tm* lt;
-    const char* p;
-
-    if (bytes == NULL)
-        return (-1);
-
-    /* Constrain to valid lengths. */
-    if (len != UTCTIME_LENGTH && len != GENTIME_LENGTH)
-        return (-1);
-
-    lt = tm;
-    if (lt == NULL) {
-        memset(&ltm, 0, sizeof(ltm));
-        lt = &ltm;
-    }
-
-    /* Timezone is required and must be GMT (Zulu). */
-    if (bytes[len - 1] != 'Z')
-        return (-1);
-
-    /* Make sure everything else is digits. */
-    for (i = 0; i < len - 1; i++) {
-        if (isdigit((unsigned char) bytes[i]))
-            continue;
-        return (-1);
-    }
-
-    /*
-     * Validate and convert the time
-     */
-    p = bytes;
-    switch (len) {
-    case GENTIME_LENGTH:
-        if (mode == V_ASN1_UTCTIME)
-            return (-1);
-        lt->tm_year = (ATOI2(p) * 100) - 1900; /* cc */
-        type = V_ASN1_GENERALIZEDTIME;
-        /* FALLTHROUGH */
-    case UTCTIME_LENGTH:
-        if (type == 0) {
-            if (mode == V_ASN1_GENERALIZEDTIME)
-                return (-1);
-            type = V_ASN1_UTCTIME;
-        }
-        lt->tm_year += ATOI2(p); /* yy */
-        if (type == V_ASN1_UTCTIME) {
-            if (lt->tm_year < 50)
-                lt->tm_year += 100;
-        }
-        lt->tm_mon = ATOI2(p) - 1; /* mm */
-        if (lt->tm_mon < 0 || lt->tm_mon > 11)
-            return (-1);
-        lt->tm_mday = ATOI2(p); /* dd */
-        if (lt->tm_mday < 1 || lt->tm_mday > 31)
-            return (-1);
-        lt->tm_hour = ATOI2(p); /* HH */
-        if (lt->tm_hour < 0 || lt->tm_hour > 23)
-            return (-1);
-        lt->tm_min = ATOI2(p); /* MM */
-        if (lt->tm_min < 0 || lt->tm_min > 59)
-            return (-1);
-        lt->tm_sec = ATOI2(p); /* SS */
-        /* Leap second 60 is not accepted. Reconsider later? */
-        if (lt->tm_sec < 0 || lt->tm_sec > 59)
-            return (-1);
-        break;
-    default:
-        return (-1);
-    }
-
-    return type;
-}
-}
-#endif /* EMBEDDED_ASN1_TIME_PARSE */
 
 namespace dht {
 namespace http {
