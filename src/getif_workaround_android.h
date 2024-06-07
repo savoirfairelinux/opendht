c03ce419cab77e8dc499a473bcf7de1dfc8aa9e6
Lanius-collaris <55432068+Lanius-collaris@users.noreply.github.com>
2024-06-07 14:53:59 +0800

    peer_discovery: add a workaround for Android hotspot

---
+++ src/getif_workaround_android.h
@@ -0,0 +1,39 @@
+//Code from getifaddrs.3 man
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ifaddrs.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <asio.hpp>
+#include <stdexcept>
+namespace dht{
+namespace workaround{
+
+asio::ip::address_v4
+get_interface(){
+    struct ifaddrs *ifaddr;
+    if(getifaddrs(&ifaddr) == -1){
+        throw std::runtime_error("Can't getifaddrs");
+    }
+    for (struct ifaddrs *if1 = ifaddr; if1 != NULL; if1 = if1->ifa_next){
+        if (if1->ifa_addr == NULL)
+            continue;
+        if (if1->ifa_addr->sa_family != AF_INET)
+            continue;
+        if((if1->ifa_flags & IFF_MULTICAST) == IFF_MULTICAST){
+            char* sa_data = if1->ifa_addr->sa_data;
+            uint32_t value = sa_data[2] << 24
+                | sa_data[3] << 16
+                | sa_data[4] << 8
+                | sa_data[5];
+            freeifaddrs(ifaddr);
+            return asio::ip::address_v4(value);
+        }
+    }
+    freeifaddrs(ifaddr);
+    throw std::runtime_error("Can't find a interface which supports multicast");
+}
+
+} /* namespace workaround */
+} /* namespace dht */
--- src/peer_discovery.cpp
+++ src/peer_discovery.cpp
@@ -22,6 +22,10 @@
 #include "network_utils.h"
 #include "utils.h"
 
+#ifdef __ANDROID__
+#include "getif_workaround_android.h"
+#endif
+
 #include <asio.hpp>
 
 using namespace std::literals;
@@ -91,6 +95,19 @@ PeerDiscovery::DomainPeerDiscovery::DomainPeerDiscovery(asio::ip::udp domain, in
         sockFd_.set_option(asio::ip::udp::socket::reuse_address(true));
         sockFd_.bind({domain, port});
     } catch (const std::exception& e) {
+#ifdef __ANDROID__
+        if(domain.family() == AF_INET && e.what() == "No such device"){
+            try{
+                auto my_interface = workaround::get_interface();
+                sockFd_.set_option(asio::ip::multicast::outbound_interface(my_interface));
+                sockFd_.set_option(asio::ip::multicast::join_group(sockAddrSend_.address().to_v4(), my_interface));
+                sockFd_.bind({domain, port});
+            }catch(const std::exception& e){
+                if (logger_)
+                    logger_->e("android discovery workaround: %s",e.what());
+            }
+        }
+#endif
         if (logger_)
             logger_->e("Can't start peer discovery for %s: %s",
                     domain.family() == AF_INET ? "IPv4" : "IPv6", e.what());
