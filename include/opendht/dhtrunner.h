e4622f5e49b058d3bc02d07cb107d8354fb97c25
Adrien Béraud <>
2025-05-19 18:08:18 -0400

    dhtrunner: add putEncrypted with PkId 'to' argument

--- include/opendht/dhtrunner.h
+++ include/opendht/dhtrunner.h
@@ -268,11 +268,17 @@ public:
         putEncrypted(hash, to, value, bindDoneCb(cb), permanent);
     }
 
+    [[deprecated("Use the shared_ptr version instead")]]
     void putEncrypted(InfoHash hash, const std::shared_ptr<crypto::PublicKey>& to, Value&& value, DoneCallback cb={}, bool permanent = false);
+    [[deprecated("Use the shared_ptr version instead")]]
     void putEncrypted(InfoHash hash, const std::shared_ptr<crypto::PublicKey>& to, Value&& value, DoneCallbackSimple cb, bool permanent = false) {
         putEncrypted(hash, to, std::forward<Value>(value), bindDoneCb(cb), permanent);
     }
 
+    void putEncrypted(InfoHash hash, const PkId& to, std::shared_ptr<Value> value, DoneCallback cb={}, bool permanent = false);
+    void putEncrypted(InfoHash hash, const PkId& to, std::shared_ptr<Value> value, DoneCallbackSimple cb, bool permanent = false) {
+        putEncrypted(hash, to, value, bindDoneCb(cb), permanent);
+    }
 
     /**
      * Insert known nodes to the routing table, without necessarly ping them.
