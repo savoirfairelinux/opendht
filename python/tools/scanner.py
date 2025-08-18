a957cdba86c1d3a01f57e8b6b748136a1d807cac
Adrien Béraud <>
2025-08-18 09:29:25 -0400

    python: update copyright

--- python/tools/scanner.py
+++ python/tools/scanner.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-# Copyright (c) 2015-2023 Savoir-faire Linux Inc.
-# Author: Adrien Béraud <adrien.beraud@savoirfairelinux.com>
+# Copyright (C) 2014-2025 Savoir-faire Linux Inc.
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import time, sys, os
 from pprint import pprint
