08b556b0dd7ec559ffbf2d1a0b47908afb704600
Adrien Béraud <>
2025-11-10 23:02:50 -0500

    base64: use simdutf

--- src/base64.cpp
+++ src/base64.cpp
@@ -17,127 +17,30 @@
 
 #include "base64.h"
 
+#include <simdutf.h>
+
+#include <vector>
 #include <cstdint>
 #include <cstdlib>
-
-/* Mainly based on the following stackoverflow question:
- * http://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c
- */
-static const char encoding_table[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-    'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
-    '3', '4', '5', '6', '7', '8', '9', '+', '/'
-};
-
-static const size_t mod_table[] = { 0, 2, 1 };
-
-char *base64_encode(const uint8_t *input, size_t input_length,
-                             char *output, size_t *output_length)
-{
-    size_t i, j;
-    size_t out_sz = *output_length;
-    *output_length = 4 * ((input_length + 2) / 3);
-    if (out_sz < *output_length || output == nullptr)
-        return nullptr;
-
-    for (i = 0, j = 0; i < input_length; ) {
-        uint8_t octet_a = i < input_length ? input[i++] : 0;
-        uint8_t octet_b = i < input_length ? input[i++] : 0;
-        uint8_t octet_c = i < input_length ? input[i++] : 0;
-
-        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
-
-        output[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
-        output[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
-        output[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
-        output[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
-    }
-
-    for (i = 0; i < mod_table[input_length % 3]; i++)
-        output[*output_length - 1 - i] = '=';
-
-    return output;
-}
-
-uint8_t *base64_decode(const char *input, size_t input_length,
-                             uint8_t *output, size_t *output_length)
-{
-    size_t i, j;
-    uint8_t decoding_table[256];
-
-    uint8_t c;
-    for (c = 0; c < 64; c++)
-        decoding_table[static_cast<int>(encoding_table[c])] = c;
-
-    if (input_length % 4 != 0 || input_length < 2)
-        return nullptr;
-
-    size_t out_sz = *output_length;
-    *output_length = input_length / 4 * 3;
-    if (input[input_length - 1] == '=')
-        (*output_length)--;
-    if (input[input_length - 2] == '=')
-        (*output_length)--;
-
-    if (out_sz < *output_length || output == nullptr)
-        return nullptr;
-
-    for (i = 0, j = 0; i < input_length;) {
-        uint8_t sextet_a = input[i] == '=' ? 0 & i++
-            : decoding_table[static_cast<int>(input[i++])];
-        uint8_t sextet_b = input[i] == '=' ? 0 & i++
-            : decoding_table[static_cast<int>(input[i++])];
-        uint8_t sextet_c = input[i] == '=' ? 0 & i++
-            : decoding_table[static_cast<int>(input[i++])];
-        uint8_t sextet_d = input[i] == '=' ? 0 & i++
-            : decoding_table[static_cast<int>(input[i++])];
-
-        uint32_t triple = (sextet_a << 3 * 6) +
-                          (sextet_b << 2 * 6) +
-                          (sextet_c << 1 * 6) +
-                          (sextet_d << 0 * 6);
-
-        if (j < *output_length)
-            output[j++] = (triple >> 2 * 8) & 0xFF;
-        if (j < *output_length)
-            output[j++] = (triple >> 1 * 8) & 0xFF;
-        if (j < *output_length)
-            output[j++] = (triple >> 0 * 8) & 0xFF;
-    }
-
-    return output;
-}
-
-std::string
-base64_encode(const std::vector<uint8_t>::const_iterator begin,
-              const std::vector<uint8_t>::const_iterator end)
-{
-    size_t output_length = 4 * ((std::distance(begin, end) + 2) / 3);
-    std::string out;
-    out.resize(output_length);
-    base64_encode(&(*begin), std::distance(begin, end),
-                  &(*out.begin()), &output_length);
-    out.resize(output_length);
-    return out;
-}
-
 
 std::string
 base64_encode(const std::vector<unsigned char>& str)
 {
-    return base64_encode(str.cbegin(), str.cend());
+    std::string buffer(simdutf::base64_length_from_binary(str.size()), '\0');
+    simdutf::binary_to_base64((const char*)str.data(), str.size(), buffer.data());
+    return buffer;
 }
 
 std::vector<unsigned char>
 base64_decode(const std::string& str)
 {
-    size_t output_length = str.length() / 4 * 3 + 2;
-    std::vector<uint8_t> output;
-    output.resize(output_length);
-    base64_decode(str.data(), str.size(), output.data(), &output_length);
-    output.resize(output_length);
-    return output;
+    std::vector<unsigned char> buffer(simdutf::maximal_binary_length_from_base64(str.data(), str.size()));
+    simdutf::result r = simdutf::base64_to_binary(str.data(), str.size(), (char *)buffer.data());
+    if(r.error) {
+        // Decoding error, return an empty vector
+        return std::vector<unsigned char>();
+    } else {
+        buffer.resize(r.count); // resize the buffer according to actual number of bytes
+    }
+    return buffer;
 }
