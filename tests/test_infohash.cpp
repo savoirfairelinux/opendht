6a88f47704966a27f603f05faa8164759c3d9dfb
Adrien Béraud <adrien.beraud@savoirfairelinux.com>
2026-03-08 18:14:33 -0400

    infohash: make core APIs constexpr
    Make Hash default constructor, hex constructor, data(), const
    operator[] and fromString constexpr. Change fromString to be
    non-throwing: it now uses a validity flag and fills the hash with zeros
    on parse error instead of throwing. Add a constexpr toHexArray helper
    that returns a std::array<char, N*2> for compile-time hex conversion.
    Update tests to include <string_view>, use std::string_view, add
    constexpr InfoHash usage and a static_assert for the hex array.

--- tests/test_infohash.cpp
+++ tests/test_infohash.cpp
@@ -6,6 +6,7 @@
 // std
 #include <iostream>
 #include <string>
+#include <string_view>
 
 // opendht
 #include "opendht/infohash.h"
@@ -21,47 +22,60 @@ void
 InfoHashTester::testConstructors()
 {
     // Default constructor creates a null infohash
-    auto nullHash = dht::InfoHash();
+    constexpr auto nullHash = dht::InfoHash();
     CPPUNIT_ASSERT_EQUAL((size_t) 20u, nullHash.size());
     CPPUNIT_ASSERT(!nullHash);
     // Build from a uint8_t. if length to short, should get a null infohash
-    uint8_t to_short[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8};
-    auto infohash = dht::InfoHash(to_short, 8);
+    constexpr uint8_t too_short[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8};
+    auto infohash = dht::InfoHash(too_short, 8);
     CPPUNIT_ASSERT_EQUAL((size_t) 20u, infohash.size());
     CPPUNIT_ASSERT_EQUAL(std::string("0000000000000000000000000000000000000000"), infohash.toString());
     // Build from a uint8_t. if length is enough, data should contains the uint8_t
-    uint8_t enough[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
-                        0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa};
+    constexpr uint8_t enough[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa,
+                                  0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa};
     infohash = dht::InfoHash(enough, 20);
-    CPPUNIT_ASSERT(infohash.size() == 20);
+    static_assert(dht::InfoHash(enough, 20) != nullHash);
+    CPPUNIT_ASSERT_EQUAL((size_t) 20u, infohash.size());
     const auto* data = infohash.data();
     for (auto i = 0; i < 20; ++i) {
         CPPUNIT_ASSERT_EQUAL(enough[i], data[i]);
     }
     // if too long, should be cutted to 20
-    uint8_t tooLong[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0x1,
-                         0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb0};
+    constexpr uint8_t tooLong[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0x1,
+                                   0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb0};
     infohash = dht::InfoHash(tooLong, 21);
-    CPPUNIT_ASSERT(infohash.size() == 20);
     const auto* data2 = infohash.data();
     for (auto i = 0; i < 20; ++i) {
         CPPUNIT_ASSERT_EQUAL(enough[i], data2[i]);
     }
     // Build from string
-    auto infohashFromStr = dht::InfoHash("0102030405060708090A0102030405060708090A");
+    constexpr auto infohashFromStr = dht::InfoHash("0102030405060708090A0102030405060708090A");
     CPPUNIT_ASSERT_EQUAL((size_t) 20u, infohashFromStr.size());
     const auto* dataStr = infohashFromStr.data();
     for (auto i = 0; i < 20; ++i) {
         CPPUNIT_ASSERT_EQUAL((int) dataStr[i], (int) data[i]);
     }
+
+    // invalid strings
+    constexpr auto invalidHash = dht::InfoHash("invalid_string0102030405060708090A010203");
+    CPPUNIT_ASSERT_EQUAL((size_t) 20u, invalidHash.size());
+    CPPUNIT_ASSERT(!invalidHash);
+    CPPUNIT_ASSERT_EQUAL(nullHash, invalidHash);
+    static_assert(invalidHash == nullHash, "Invalid hash string should result in null hash");
+
+    constexpr auto tooShortHash = dht::InfoHash("0123456789abcdef0123456789abcdef012345");
+    CPPUNIT_ASSERT_EQUAL((size_t) 20u, tooShortHash.size());
+    CPPUNIT_ASSERT(!tooShortHash);
+    CPPUNIT_ASSERT_EQUAL(nullHash, tooShortHash);
+    static_assert(tooShortHash == nullHash, "Too short hash string should result in null hash");
 }
 
 void
 InfoHashTester::testComparators()
 {
-    auto nullHash = dht::InfoHash();
-    auto minHash = dht::InfoHash("0000000000000000000000000000000000111110");
-    auto maxHash = dht::InfoHash("0111110000000000000000000000000000000000");
+    constexpr auto nullHash = dht::InfoHash();
+    constexpr auto minHash = dht::InfoHash("0000000000000000000000000000000000111110");
+    constexpr auto maxHash = dht::InfoHash("0111110000000000000000000000000000000000");
     // operator ==
     CPPUNIT_ASSERT_EQUAL(minHash, minHash);
     CPPUNIT_ASSERT_EQUAL(minHash, dht::InfoHash("0000000000000000000000000000000000111110"));
@@ -126,10 +140,21 @@ InfoHashTester::testXorCmp()
 void
 InfoHashTester::testHex()
 {
-    const std::string TEST_HASH_STR("01b20304d5060708090a010203e05060708090ae");
+    using namespace std::literals;
+    static constexpr auto H = "0123456789abcdef0123456789abcdef01234567"sv;
+    const std::string TEST_HASH_STR(H);
     dht::InfoHash TEST_HASH(TEST_HASH_STR);
+    static constexpr dht::InfoHash TEST_HASH_CONST(H);
     CPPUNIT_ASSERT_EQUAL(TEST_HASH_STR, TEST_HASH.toString());
+    CPPUNIT_ASSERT_EQUAL(H, TEST_HASH.to_view());
     CPPUNIT_ASSERT_EQUAL(TEST_HASH_STR, dht::toHex(TEST_HASH.data(), TEST_HASH.size()));
+    CPPUNIT_ASSERT_EQUAL(TEST_HASH_CONST.to_view(), H);
+
+    static constexpr auto hexArray = dht::toHexArray(TEST_HASH_CONST);
+    static constexpr auto hexArrayView = std::string_view(hexArray.data(), hexArray.size());
+    static_assert(hexArrayView == H, "Hex array does not match");
+
+    CPPUNIT_ASSERT_EQUAL(std::string_view(TEST_HASH_STR), hexArrayView);
 }
 
 void
