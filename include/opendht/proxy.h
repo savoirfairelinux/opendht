3149fe60c4c465e2dc8e25db9685e30c1440791c
Adrien Beraud <adrien.beraud@savoirfairelinux.com>
2026-09-03 13:51:53 -0400

    proxy: increase OP_MARGIN to 8 hours

--- include/opendht/proxy.h
+++ include/opendht/proxy.h
@@ -8,7 +8,7 @@ namespace dht {
 namespace proxy {
 
 constexpr const std::chrono::hours OP_TIMEOUT {24}; // one day
-constexpr const std::chrono::hours OP_MARGIN {2};   // two hours
+constexpr const std::chrono::hours OP_MARGIN {8};   // eight hours
 
 using ListenToken = uint64_t;
 
